0c069fbabc63e734c3c1121d78d66a686c46a29c
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-04-01 07:22:26 -0700

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,26 @@
+require 'pry'
 
+class CashRegister
+	
+	def initialize(discount = 0)
+		@total = total
+		@discount = discount
+	end
+	
+	def total
+		@total = 0
+	end
+	
+	def total= (total)
+		@total = total
+	end
+	binding.pry
+	def discount
+		@discount
+	end
+
+end
+
+cash_register = CashRegister.new
+cash_register.total = 100
+cash_register.total
